40335df8416087e6ea384b73598bb130495bb0bb
Benjamin THOMAS <benjamin.thomas@ynov.com>
2017-02-10 18:04:16 +0100

    repush history thomas

--- WinAppBJ/WinAppBJ/ViewModel/RoomViewModel.cs
+++ WinAppBJ/WinAppBJ/ViewModel/RoomViewModel.cs
@@ -6,14 +6,18 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace WinAppBJ.ViewModel
 {
     class RoomViewModel
     {
+        Frame actualFrame { get { return Window.Current.Content as Frame; } }
         public Room Rooms { get; set; }
         public List<User> Onlines { get; set; }
 
@@ -64,5 +68,43 @@ namespace WinAppBJ.ViewModel
                 }
             }
         }
+
+        public async void GetOpenedTable()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://demo.comte.re");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("/api/table/opened");
+                if (response.IsSuccessStatusCode)
+                {
+                    string res = await response.Content.ReadAsStringAsync();
+                }
+            }
+        }
+
+        public async void DeconnectUser(User user)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://demo.comte.re");
+
+                var json = JsonConvert.SerializeObject(user.email);
+                var itemJson = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("/api/auth/logout", itemJson);
+                if (response.IsSuccessStatusCode)
+                {
+                    var dialog = new MessageDialog("Vous avez été déconnecté");
+                    await dialog.ShowAsync();
+
+                    actualFrame.Navigate(typeof(LoginPage));
+
+
+                }
+            }
+        }
     }
 }
+
