170f3305ecd75b92bc07ab1947b563313eb075be
ThomasMoiroud <thomas.moiroud@ynov.com>
2017-02-11 16:24:39 +0100

    table User + call Api table Libre

--- WinAppBJ/WinAppBJ/ViewModel/RoomViewModel.cs
+++ WinAppBJ/WinAppBJ/ViewModel/RoomViewModel.cs
@@ -20,10 +20,13 @@ namespace WinAppBJ.ViewModel
         Frame actualFrame { get { return Window.Current.Content as Frame; } }
         public Room Rooms { get; set; }
         public List<User> Onlines { get; set; }
+        public Table Tables { get; set; }
+        public List<Table> Free { get; set; }
 
         public RoomViewModel()
         {
             Onlines = new List<User>();
+            Free = new List<Table>();
             getPlayerOnline();
 
         }
@@ -74,13 +77,25 @@ namespace WinAppBJ.ViewModel
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("http://demo.comte.re");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("accept", "application/json");
+                client.DefaultRequestHeaders.Add("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjRmNzU2ZmJmYTc2ZWVjZjRkMTdhMjcxNjgwZTEzZGNmODBiYTEwMWQ4MzI3YjczMmQyNzMyNjdkODMxNzA1ZmY2OTM2ZTE4ZDVmOTg1MmU0In0.eyJhdWQiOiJ0ZXN0QHlub3YuY29tIiwianRpIjoiNGY3NTZmYmZhNzZlZWNmNGQxN2EyNzE2ODBlMTNkY2Y4MGJhMTAxZDgzMjdiNzMyZDI3MzI2N2Q4MzE3MDVmZjY5MzZlMThkNWY5ODUyZTQiLCJpYXQiOjE0ODY3NDUyNjgsIm5iZiI6MTQ4Njc0NTI2OCwiZXhwIjoxNTE4MjgxMjY4LCJzdWIiOiIyNCIsInNjb3BlcyI6WyIqIl19.BhFaaCMNCRGMMHaEshKzQSkRIPkz5dS0XjeLqZlegSxTmB2unTqurHR_ODA6HWw1bwlBq7f4DoUGmJAq3YCDU88GMj3Fnxtinb4Q0EDlIHqwqKnMWOeSS4WUccvtaO4QIIpWa2ynrxajd49w8OAjaJTjWqQJ-Y2N8-zsSkQ_LMtjtkTEt0pjMckXIVkMjGF_MDGWA87clP0g_bp1WnxTuLB7HcqPZzcGMWi6CkPGlkRmYrnJpAxbBzlrDOKDi-1u7N5eR9WIOHF0E-jHktQTbl-FnJa3Fq8blZmEPTZ0l5alLUcaGnCo6P6YgDOGoamdnrUVsIfCc-x4A7wrBvmc7iMSGPLao9wyDUsticYVVRHGBKOHVzHW1PXb506vtu7FYTazYLgEtq9CPfFTI3k8vFqmj51k7ayCTHwGoWQIV0Ztk09Tk1bpSMQMDiwvzbN59Uxm07m0TTCgWLHBbHq3kvyYJJmUNahasGsV3pBv2K9UF0obzyPZP2cLtgvvrJd9h9El7-1EZZdWQLxwj52hVhcEFRgDT1HxbQxLWtJjuVI4ccHsubFaJxBoDyFaqzBRkwFfxcnwTInahErzZo8-qok6BM9sbOMkWYu2VtL18e7AZcFuFV1OpqkG2rIhRUoKQqOFiMJ9tlKHitpTucPjv7bcg_1wt5ElVgIKLa3DbX4");
 
                 HttpResponseMessage response = await client.GetAsync("/api/table/opened");
                 if (response.IsSuccessStatusCode)
                 {
                     string res = await response.Content.ReadAsStringAsync();
+                    var jobject = JObject.Parse(res);
+                    var resultsTables = jobject["tables"];
+
+                    for (int i = 0; i < resultsTables.Count(); i++)
+                    {
+                        var item = resultsTables.ElementAt(i);
+                        Table t = new Table();
+                        {
+                            id = item["id"],
+                        };
+                        Free.Add(t);
+                    }
                 }
             }
         }
